366c1ecf924f68593dec470c45de4b0a7e28586c
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2023-11-22 18:14:17 -0500

    v4.2

--- src/index.js
+++ src/index.js
@@ -17,13 +17,29 @@ const App = () => {
     setRight(right + 1)
   }
 
+  const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+  
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
+  }  
+
   return (
     <div>
       {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
