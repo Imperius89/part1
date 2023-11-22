4fc660fdd620fb92071d1d66f2a7b74f9aa9b794
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2023-11-21 19:27:03 -0500

    v2 corrected

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {useState} from 'react'
+
+ReactDOM.render(<App />, document.getElementById('root')) 
+
 const App = () => {
   const [clicks, setClicks] = useState({
     left: 0, right: 0
   })
 
-  const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
-  }
+  const handleLeftClick = () =>
+  setClicks({ ...clicks, left: clicks.left + 1 })
 
-  const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
-  }
-
+const handleRightClick = () =>
+  setClicks({ ...clicks, right: clicks.right + 1 })
+  
   return (
     <div>
       {clicks.left}
