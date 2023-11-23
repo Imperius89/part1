d142af40b877ece7d124a3155072c2e1c81ef712
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2023-11-22 20:11:19 -0500

    v1.3

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const App = () => {
     <div>
       {value}
 
-      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
       <button onClick={setToValue(0)}>reset</button>
       <button onClick={setToValue(value + 1)}>increment</button>
     </div>
