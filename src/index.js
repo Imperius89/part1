9b5b7ab637ef65a76ab471f54d8bb6e29313e6b9
Oscar David Becerra Mejia <oscardbecerram@gmail.com>
2023-11-22 19:43:19 -0500

    v1

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import {useState} from 'react'
 
 const App = () => {
   const [value, setValue] = useState(10)
-  
-  const handleClick = () => {
-    console.log('clicked the button')
-    setValue(0)
+
+
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who)
+    }
+    return handler
   }
 
   return (
     <div>
       {value}
-      <button onClick={handleClick}>button</button>
+
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </div>
   )
 }
